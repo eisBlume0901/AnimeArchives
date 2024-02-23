0e72345ad695e0acb8d97db597a7538f2184e191
Mary Kate F. Anecito <marykate09@protonmail.com>
2024-02-23 15:36:20 +0800

    Updated name of the project from AniMaKer to AnimeArchives along with the database credentials.

--- database/migrations/2024_02_23_071442_create_anime_list_table.php
+++ database/migrations/2024_02_23_071442_create_anime_list_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('anime_list', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->text('genre');
+            $table->date('start_aired_date');
+            $table->date('end_aired_date');
+            $table->string('website');
             $table->timestamps();
         });
     }
