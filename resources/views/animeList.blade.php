87df5908a0c4fa786370a52a2e20473fea7b81f9
Mary Kate F. Anecito <marykate09@protonmail.com>
2024-02-27 12:38:59 +0800

    Added the index.html layout to the resources > views. Used Eloquent ORM to fetch data from the database and display it in the view.

--- resources/views/animeList.blade.php
+++ resources/views/animeList.blade.php
@@ -1,18 +1,84 @@
 @extends('layout')
 
 @section('content')
-
-<h1>{{$heading}}</h1>
-
+@include('partials._search')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 @if(count($animeList) == 0)
     <h2>No listings found</h2>
 @endif
 
 @foreach ($animeList as $animeElement)
-    <h2>
-        <a href="/anime/{{$animeElement['id']}}">{{$animeElement['title']}}</a>
-    </h2>
-    <p>{{$animeElement['description']}}</p>
-@endforeach
+        <div class="bg-gray-50 border border-gray-200 rounded p-6 hover:bg-customLightPink">
+            <div class="flex">
+{{--                Have to update the hidden attribute--}}
+                <img
+                    class="hidden w-48 mr-6 md:block"
+                    src="Frieren Beyond Journey's End.jpg"
+                    alt=""
+                />
+                <div>
+                    <div class="">
+                        <h3 class="text-2xl hover:text-customDarkPink">
+{{--                            Instead of using the [] notation (i.e., {{$animeElement['title']}}),--}}
+{{--                            we can use the Eloquent model to fetch data from the database.--}}
+{{--                            This is a more object-oriented approach and is the recommended way to--}}
+{{--                            fetch data from the database.--}}
+                            <a href="./anime/{{$animeElement->id}}">{{$animeElement->title}}</a>
+                        </h3>
+                    </div>
+                    <div class="text-md mt-2 mb-3">{{$animeElement->episodes}} Episodes</div>
 
+                    <ul class="flex">
+                        <li
+                            class="flex items-center justify-center bg-customPurple text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                        >
+                            <a href="#">Adventure</a>
+                        </li>
+                        <li
+                            class="flex items-center justify-center bg-customPurple text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                        >
+                            <a href="#">Action</a>
+                        </li>
+                        <li
+                            class="flex items-center justify-center bg-customPurple text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                        >
+                            <a href="#">Drama</a>
+                        </li>
+                        <li
+                            class="flex items-center justify-center bg-customPurple text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                        >
+                            <a href="#">Fantasy</a>
+                        </li>
+                    </ul>
+                    <div class="mt-4">
+                        <h4 class="mb-2 text-lg">Streaming Platforms</h4>
+                        <ul class="flex flex-row gap-3">
+                            <li>
+                                <a href="https://www.crunchyroll.com"><img src="https://cdn.iconscout.com/icon/free/png-512/crunchyroll-4062809-3357695.png"
+                                                                           alt="Crunchyroll logo" width="30" class="rounded-2xl bg-white border-2 border-customLightPink"></a>
+                            </li>
+                            <li>
+                                <a href="https://www.netflix.com"><img src="https://vignette2.wikia.nocookie.net/logopedia/images/b/b2/NetflixIcon2016.jpg/revision/latest/scale-to-width-down/2000?cb=20160620223003"
+                                                                       alt="Netflix logo" width="30" class="rounded-2xl bg-white border-2 border-customLightPink"></a>
+                            </li>
+                            <li>
+                                <a href=""><img src="https://th.bing.com/th/id/OIP.BZDUzewveeEejbZ5rdSetQHaHa?rs=1&pid=ImgDetMain"
+                                                alt="Aniplus TV logo" width="30" back class="rounded-2xl bg-white border-2 border-customLightPink"></a>
+                            </li>
+                            <li>
+                                <a href=""><img src="https://img.icons8.com/color/452/bilibili.png" alt="Bilibili Global logo" width="30" class="rounded-2xl bg-white border-2 border-customLightPink"></a>
+                            </li>
+                            <li>
+                                <a href=""><img src="https://th.bing.com/th/id/OIP.PCg0iBIJYDpjE6ReDPSB1gHaHa?rs=1&pid=ImgDetMain" alt="Muse Asia logo" width="30" class="rounded-2xl bg-white border-2 border-customLightPink"></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="text-sm mt-4 mx-1.5">
+                        <i class="fa-solid fa-tv mr-1"></i>{{$animeElement->broadcast}}
+                    </div>
+                </div>
+            </div>
+        </div>
+@endforeach
+    </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,16 +1,73 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AnimeArchives</title>
-<head>
-<body>
-<h1>AnimeArchives</h1>
-{{--VIEW OUTPUT--}}
-@yield('content')
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="images/favicon.ico" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+    />
+    <!--            font awesome cheat sheet https://fontawesome.com/v4/cheatsheet/-->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        customPurple: "#211951",
+                        customDarkPink: "#FF2E9F",
+                        customLightPink: "#FFCAD4"
+                    },
+                },
+            },
+        };
+    </script>
+    <title>Anime Archives | Anime Search Engine</title>
+</head>
+<body class="mb-48">
+<nav class="flex justify-between items-center mb-4 bg-customPurple">
+{{--    We have to link back to our home page which is the animeList.blade.php template--}}
+    <a href="/"
+{{--       Instead of using images/logo.png, we can use the asset() function to fetch the image from the public directory.--}}
+    ><img class="w-40 mx-10" src="{{asset('images/logo.png')}}" alt="" class="logo"
+        /></a>
+    <ul class="flex flex-col mx-10 text-lg">
+        <li class="my-1.5">
+            <a href="register.html" class="text-white"
+            ><i class="mx-1.5 fa-solid fa-user text-white"></i> Register</a
+            >
+        </li>
+        <li class="my-1.5">
+            <a href="login.html" class="text-white"
+            ><i class="mx-1.5 fa-solid fa-arrow-right-to-bracket text-white"></i>
+                Login</a
+            >
+        </li>
+    </ul>
+</nav>
+<main>
+    @yield('content')
+</main>
 </body>
+<footer
+    class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-customPurple text-white h-24 mt-24 opacity-90 md:justify-center"
+>
+    <p class="ml-2">Copyright &copy; 2024, All Rights reserved</p>
+
+    <a
+        href="create.html"
+        class="absolute top-1/3 right-10 bg-customPurple text-white py-2 px-5 hover:bg-customDarkPink hover:text-white rounded-xl"
+    >Post Anime</a
+    >
+</footer>
+</body>
+</html>
+
 </html>
 
 
